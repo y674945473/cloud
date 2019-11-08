771d646a0f9fd43e949000b1c8d8d7b7ad63f5cf
yhx <674945473@qq.com>
2019-11-08 15:46:35 +0800

    配置提交

--- quarzt/src/main/java/org/quarzt/QuarztApplication.java
+++ quarzt/src/main/java/org/quarzt/QuarztApplication.java
@@ -4,14 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableEurekaClient
+@ComponentScan(value="org.quarzt.dao.JobEntityRepository")
 public class QuarztApplication 
 {
 	private static Logger Logger = LoggerFactory.getLogger(QuarztApplication.class);
--- quarzt/src/main/java/org/quarzt/dao/JobEntityRepository.java
+++ quarzt/src/main/java/org/quarzt/dao/JobEntityRepository.java
@@ -2,10 +2,12 @@ package org.quarzt.dao;
 
 import org.quarzt.entity.JobEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by EalenXie on 2018/6/4 14:27
  */
+@Component
 public interface JobEntityRepository extends CrudRepository<JobEntity, Long> {
     JobEntity getById(Integer id);
 }
